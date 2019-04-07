31bb5340f352c417ec8c19903459b87678782301
Harry Apriadi <harryapriadi1@gmail.com>
2019-04-07 21:07:03 +0700

    LAT_AKBIF9_10116413_HARRYAPRIADI

---
+++ app/src/main/java/com/example/lat_akbif9_10116413_harryapriadi/harinamaku.java
@@ -0,0 +1,13 @@
+package com.example.lat_akbif9_10116413_harryapriadi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class harinamaku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harinamaku);
+    }
+}
